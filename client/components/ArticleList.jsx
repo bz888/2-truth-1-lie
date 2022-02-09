3bced0c28565388a750deb5479b6c792bf5419c3
bz888 <ziyu.ben158@gmail.com>
2022-02-09 20:31:56 +1300

    fixed bug

--- client/components/ArticleList.jsx
+++ client/components/ArticleList.jsx
@@ -12,7 +12,7 @@ import LoadAnim from './LoadAnim'
 
 function ArticleList () {
   const tempRef = query(collection(getFirestore(), 'test_read'), orderBy('timestamp', 'desc'), limit(2))
-  const [userArticles, loading, error] = useCollectionData(tempRef, { idField: 'id' })
+  const [userArticles, loading, error] = useCollectionData(tempRef)
   console.log(userArticles)
   return (
     <>
@@ -23,7 +23,7 @@ function ArticleList () {
           return (
 
             <Article
-              key={idx + 1}
+              key={dataObj.id}
               name={dataObj.name}
               article={dataObj.article}
               time={dataObj.timestamp}
