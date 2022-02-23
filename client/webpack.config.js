51bfcdaaefd2d700edaf333ca1c5d15d9240d0c3
bz888 <ziyu.ben158@gmail.com>
2022-02-24 05:25:46 +1300

    cant tell if its working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path')
-const Dotenv = require('dotenv-webpack')
+// const Dotenv = require('dotenv-webpack')
+const { DefinePlugin } = require('webpack')
 module.exports = (env) => {
   console.log('env: ', env)
   // target: 'node',
   return {
     plugins: [
-      new Dotenv({
-        path: path.resolve(__dirname, '../.env')
+      // new Dotenv({
+      //   path: path.resolve(__dirname, '../.env')
+      // })
+      new DefinePlugin({
+        'process.env.LOGIN_KEY': JSON.stringify('bz888dev@gmail.com'),
+        'process.env.RECAPTCHA_KEY': JSON.stringify('6Lc9tZceAAAAAOtP6LgstLdu6Kx0H_GVhBEsBW1b')
       })
     ],
     entry: path.join(__dirname, './index.js'),
