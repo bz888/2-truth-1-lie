aed0e64d0157ce8ddaa700d394a52ba68ca953b1
bz888 <ziyu.ben158@gmail.com>
2022-02-23 19:24:28 +1300

    webpack changes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
-module.exports = {
+module.exports = () => {
   // target: 'node',
-  plugins: [
-    new Dotenv()
-  ],
-  entry: path.join(__dirname, './index.js'),
-  output: {
-    path: path.join(__dirname, '../server/public'),
-    filename: 'bundle.js'
-  },
-  mode: 'development',
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    fallback: {
-      fs: false
-    }
-  },
-  devtool: 'source-map'
+  return {
+    plugins: [
+      new Dotenv()
+    ],
+    entry: path.join(__dirname, './index.js'),
+    output: {
+      path: path.join(__dirname, '../server/public'),
+      filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      fallback: {
+        fs: false
+      }
+    },
+    devtool: 'source-map'
+  }
 }
